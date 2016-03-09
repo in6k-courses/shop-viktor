54e1b248b65ebc4d2c113ea729b9856b0130441f
ViktorG-in6k <ViktorG@softwareplanet.uk.com>
2016-03-09 11:33:50 +0200

    re-format

--- src/test/java/Main/NewBasketTest.java
+++ src/test/java/Main/NewBasketTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class NewBasketTest {
 
